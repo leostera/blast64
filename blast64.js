27db565c2ed546760241228b90fd43b855bd5c01
Leandro Ostera <leandro@ostera.io>
2016-08-19 01:07:00 +0200

    Working decoder

--- blast64.js
+++ blast64.js
@@ -1,25 +1,37 @@
 var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".split("")
-var encoding_table = new ArrayBuffer(64)
-var decoding_table = new Uint8Array(64) 
+var table = new ArrayBuffer(64)
+var decoder = new TextDecoder("utf8")
 
 /* build tables on load */
 for(var i=0; i<64; i++) {
   var i = i|0
   var c = chars[i].charCodeAt(0)
-  encoding_table[ c ] = i
-  decoding_table[ i ] = c 
+  table[ c ] = i
 }
 
+// in: T  W  F  u
+// out: 77 97 110
 function decode(string) {
   "use asm";
-  var length = string.length
-  var buffer = new ArrayBuffer(string.length / 4 * 3)
-  var enc = [,,,];
-  var position = -1
+  var length = string.length | 0,
+      array = new ArrayBuffer(string.length / 4 * 3),
+      buffer = new Uint8Array(array)
+      enc = [,,,],
+      position = -1,
+      i = 0 
   while(++position < length) {
-     
+    enc[0] = table[ string[position].charCodeAt(0) ] 
+    enc[1] = table[ string[++position].charCodeAt(0) ] 
+    buffer[i++] = ( enc[0] << 2 ) | ( enc[1] >> 4 ) 
+    enc[2] = table[ string[++position].charCodeAt(0) ]
+    if( enc[2] === 64 ) {
+      break
+    }
+    buffer[i++] = ( (enc[1] & 15) << 4) | ( enc[2] >> 2 )
+    enc[3] = table[ string[++position].charCodeAt(0) ]
+    buffer[i++] = ( (enc[2] & 3) << 6 ) | enc[3]
   }
-  return buffer;
+  return decoder.decode(buffer)
 }
 
 window.blast64 = {

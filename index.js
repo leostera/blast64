8b580efb7174942d37c6d71af531f7aeda55ee55
Leandro Ostera <leandro@ostera.io>
2016-08-19 04:18:32 +0200

    fsjidao

--- index.js
+++ index.js
@@ -32,6 +32,37 @@ function decode(string) {
   return decoder.decode( new Uint8Array(array, 0, i) )
 }
 
+function asm_decode(stdlib, ffi, heap) {
+  "use asm";
+
+  function decode(string) {
+      var array  = new ArrayBuffer(string.length / 4 * 3),
+          buffer = new Uint8Array(array),
+          enc    = new Uint8Array(4),
+          position = -1,
+          i = 0
+      while(position ^ string.length) {
+        enc[0] = table[ string.charCodeAt(++position) ]
+        enc[1] = table[ string.charCodeAt(++position) ]
+        buffer[i++] = ( enc[0] << 2 ) | ( enc[1] >> 4 )
+        enc[2] = table[ string.charCodeAt(++position) ]
+        if( enc[2] & 64 )
+          break
+        buffer[i++] = ( (enc[1] & 15) << 4) | ( enc[2] >> 2 )
+        enc[3] = table[ string.charCodeAt(++position) ]
+        if( enc[3] & 64 )
+          break
+        buffer[i++] = ( (enc[2] & 3) << 6 ) | enc[3]
+      }
+      return decoder.decode( new Uint8Array(array, 0, i) )
+    }
+
+  return {
+    decode: decode
+  }
+}
+
 module.exports = {
-  decode: decode
+  decode: decode,
+  asm: asm_decode(window, null, new ArrayBuffer(0x150000))
 }
